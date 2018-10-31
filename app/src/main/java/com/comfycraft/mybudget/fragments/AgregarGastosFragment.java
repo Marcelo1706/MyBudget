489645bb71de1caef1b2a9bad77111e0c633cc6e
Oswaldo Ortiz <oswaldo.ortiz@protonmail.com>
2018-10-31 02:52:33 -0600

    Agregar registros de gastos y pagos

--- app/src/main/java/com/comfycraft/mybudget/fragments/AgregarGastosFragment.java
+++ app/src/main/java/com/comfycraft/mybudget/fragments/AgregarGastosFragment.java
@@ -1,13 +1,19 @@
 package com.comfycraft.mybudget.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.comfycraft.mybudget.R;
+import com.comfycraft.mybudget.layout.MainActivity;
+import com.comfycraft.mybudget.utilidades.Crud;
 import com.comfycraft.mybudget.utilidades.Sesiones;
 
 public class AgregarGastosFragment extends Fragment {
@@ -20,15 +26,65 @@ public class AgregarGastosFragment extends Fragment {
     private String password;
     private String id_periodo;
 
+
+    public View view;
+
+    EditText etgastos;
+    EditText etnombre_gasto;
+    Button btnNuevogasto;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //Inicializar variable de sesion
+
+        view =  inflater.inflate(R.layout.fragment_agregar_gastos, container, false);
         session = new Sesiones(getActivity().getApplicationContext());
         id_usuario = session.getUserId();
         nombre_usuario = session.getUserName();
         password = session.getPassword();
         id_periodo = session.getIdPeriodo();
-        return inflater.inflate(R.layout.fragment_agregar_gastos, container, false);
+
+        etnombre_gasto = view.findViewById(R.id.nombre_gasto);
+        etgastos = view.findViewById(R.id.monto_gasto);
+
+        //Poner los fondos en el ET
+        Crud crud = new Crud(getActivity().getApplicationContext());
+        //Cursor select  = crud.Select("periodos","id_periodo='"+id_periodo+"'");
+
+        btnNuevogasto = view.findViewById(R.id.guardar_gasto);
+
+        btnNuevogasto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertargasto(view);
+            }
+        });
+
+        //return inflater.inflate(R.layout.fragment_agregar_gastos, container, false);
+        return view;
+    }
+
+    public void insertargasto(View view){
+        //etgastos = view.findViewById(R.id.monto_gasto);
+        String nombre_gasto = etnombre_gasto.getText().toString();
+        String fondos = etgastos.getText().toString();
+
+        //Validacion
+        if(fondos.isEmpty() || Float.parseFloat(fondos) == 0){
+            Toast.makeText(getActivity().getApplicationContext(),"Debe ingresar un valor númerico mayor que 0",Toast.LENGTH_SHORT).show();
+        } else {
+            Crud insert = new Crud(getActivity().getApplicationContext());
+            String[] campos = {"nombre", "monto", "id_periodo"};
+            String[] valores = {nombre_gasto, fondos, id_periodo};
+
+            long resultado = insert.Insert("gastos",campos,valores);
+
+
+            if(resultado > 0){
+                Toast.makeText(getActivity().getApplicationContext(),"gasto Agregado",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        }
     }
 }
--- app/src/main/java/com/comfycraft/mybudget/fragments/AgregarPagosFragment.java
+++ app/src/main/java/com/comfycraft/mybudget/fragments/AgregarPagosFragment.java
@@ -1,17 +1,25 @@
 package com.comfycraft.mybudget.fragments;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.comfycraft.mybudget.R;
 import com.comfycraft.mybudget.layout.MainActivity;
 import com.comfycraft.mybudget.utilidades.Sesiones;
+import com.comfycraft.mybudget.utilidades.Crud;
 
 public class AgregarPagosFragment extends Fragment {
+
 
     //Variable de sesion
     private Sesiones session;
@@ -21,16 +29,65 @@ public class AgregarPagosFragment extends Fragment {
     private String password;
     private String id_periodo;
 
+
+    public View view;
+
+    EditText etpagos;
+    EditText etnombre_pago;
+    Button btnNuevoPago;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //Inicializar variable de sesion
+
+        view =  inflater.inflate(R.layout.fragment_agregar_pagos, container, false);
         session = new Sesiones(getActivity().getApplicationContext());
         id_usuario = session.getUserId();
         nombre_usuario = session.getUserName();
         password = session.getPassword();
         id_periodo = session.getIdPeriodo();
-        return inflater.inflate(R.layout.fragment_agregar_pagos, container, false);
 
+        etnombre_pago = view.findViewById(R.id.nombre_pago);
+        etpagos = view.findViewById(R.id.monto_pago);
+
+        //Poner los fondos en el ET
+        Crud crud = new Crud(getActivity().getApplicationContext());
+        //Cursor select  = crud.Select("periodos","id_periodo='"+id_periodo+"'");
+
+        btnNuevoPago = view.findViewById(R.id.guardar_pago);
+
+        btnNuevoPago.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertarPago(view);
+            }
+        });
+
+        //return inflater.inflate(R.layout.fragment_agregar_pagos, container, false);
+        return view;
+    }
+
+    public void insertarPago(View view){
+        //etpagos = view.findViewById(R.id.monto_pago);
+        String nombre_pago = etnombre_pago.getText().toString();
+        String fondos = etpagos.getText().toString();
+
+        //Validacion
+        if(fondos.isEmpty() || Float.parseFloat(fondos) == 0){
+            Toast.makeText(getActivity().getApplicationContext(),"Debe ingresar un valor númerico mayor que 0",Toast.LENGTH_SHORT).show();
+        } else {
+            Crud insert = new Crud(getActivity().getApplicationContext());
+            String[] campos = {"nombre", "monto", "id_periodo"};
+            String[] valores = {nombre_pago, fondos, id_periodo};
+
+            long resultado = insert.Insert("pagos",campos,valores);
+
+
+            if(resultado > 0){
+                Toast.makeText(getActivity().getApplicationContext(),"Pago Agregado",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        }
     }
 }
